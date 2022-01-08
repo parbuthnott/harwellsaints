c9ce52c4cd7830259c42a584f822d7fe52e3320a
peter arbuthnott <p.arbuthnott@elsevier.com>
2022-01-08 01:12:01 +0000

    players pages started

--- publish_flattened.sh
+++ publish_flattened.sh
@@ -1,1 +1,9 @@
 cp *.html /c/wamp64/www/harwellsaints/flattened/
+cd fixtures
+cp *.html /c/wamp64/www/harwellsaints/flattened/fixtures
+cd ../tables
+cp *.html /c/wamp64/www/harwellsaints/flattened/tables
+cd ../players
+cp *.html /c/wamp64/www/harwellsaints/flattened/players
+cd ..
+
